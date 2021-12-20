6f70aceaa9703bea0b8742276b77183901cc5948
HungHsuChen <chhchen@ucdavis.edu>
2021-12-20 12:50:28 -0800

    API after class

---
+++ MovieShop.API/Controllers/MoviesController.cs
@@ -0,0 +1,42 @@
+﻿using ApplicationCore.ServiceInterfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieShop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieService _movieService;
+
+        public MoviesController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        [HttpGet]
+        [Route("topRevenue")]
+        public async Task<IActionResult> GetTopRevenueMovies()
+        {
+            var movies = await _movieService.GetHighestGrossingMovies();
+            if (!movies.Any())
+            {
+                return NotFound();
+            }
+            return Ok(movies);
+        }
+
+        [HttpGet]
+        [Route("details/{id:int}")]
+        public async Task<IActionResult> Details(int id)
+        {
+            var movie = await _movieService.GetMovieDetail(id);
+            if (movie==null)
+            {
+                return NotFound();
+            }
+            return Ok(movie);
+        }
+    }
+}
