fc188848d3d49b546cda4c736260c5ad44dc2231
HungHsuChen <chhchen@ucdavis.edu>
2022-01-04 00:43:44 -0500

    jwt and subjects

--- MovieShopSPA/src/app/account/login/login.component.ts
+++ MovieShopSPA/src/app/account/login/login.component.ts
@@ -1,4 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/core/services/authentication.service';
+import { Login } from 'src/app/shared/models/login';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userLogin: Login = {
+    email: '',
+    password: ''
+  };
+
+  constructor(private authService: AuthenticationService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  loginSubmit(){
+    // capture the email/password from the view
+    // then send the model to Authentication Service
+
+    // console.log('login button clicked');
+    // console.log(this.userLogin);
+    this.authService.login(this.userLogin).subscribe(
+      // if token is saved successfully, then redirect to home page
+      // if error then show error message and stay on same page
+
+      (response) => {
+        if (response){
+          this.router.navigateByUrl('/');
+        }
+
+        (err: HttpErrorResponse) => {
+          console.log(err);
+        }
+      }
+    )
+  }
 }
